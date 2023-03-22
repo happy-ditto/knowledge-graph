348e5c56ac7f9f9ca886ea4ec018a8aaa0555c6d
limian686868 <mianli2@illinois.edu>
2023-03-21 22:27:21 -0400

    update biomedical data and modify the script

--- create_structured_csv.py
+++ create_structured_csv.py
@@ -17,6 +17,7 @@ def main():
 
         #add all the names in entity set
         entity_set = set(entities.keys())
+        print(entity_set)
         final_list = []
         curr_dir = os.getcwd()
         file_name_list = file.split('/')[-1].split('.')[0].split('_')[2:]
@@ -28,26 +29,34 @@ def main():
             print(file_name)
 
         df = pd.read_csv(curr_dir +"/data/output/kg/"+file_name+".txt-out.csv")
-        
+
         #parse every row present in the intermediate csv file
         triplet = set()
+        count = 0
         for i,j in df.iterrows():
+
             j[0] = j[0].strip()
+            j[0] = " ".join(j[0].split())
+        
             #if entity is present in entity set, only then parse futrther
-            if j[0] in entity_set:
-                added = False
-                e2_sentence = j[2].split(' ')
-                #check every word in entity2, and add a new row triplet if it is present in entity2
-                for entity in e2_sentence:
-                    if entity in entity_set:
-                        _ = (entities[j[0]], j[0], j[1] ,entities[entity], j[2] )
+            for entity_sample in entity_set: 
+                if entity_sample in j[0]:
+
+                    added = False
+                    #e2_sentence = j[2].split(' ')
+                    e2_sentence = " ".join(j[2].split())
+                    #check every word in entity2, and add a new row triplet if it is present in entity2
+                    for entity_sample_2 in entity_set: 
+                        if entity_sample_2 in e2_sentence:
+                            _ = (entities[entity_sample], j[0], j[1] ,entities[entity_sample_2], j[2] )
+                            triplet.add(_)
+                            added = True
+                    if not added:
+                        _ = (entities[entity_sample], j[0], j[1] ,'O', j[2] )
                         triplet.add(_)
-                        added = True
-                if not added:
-                    _ = (entities[j[0]], j[0], j[1] ,'O', j[2] )
-                    triplet.add(_)
         #convert the pandas dataframe into csv
         processed_pd = pd.DataFrame(list(triplet),columns=['Type','Entity 1','Relationship','Type', 'Entity2'])
+        
         processed_pd.to_csv('./data/result/' + file.split("/")[-1].split(".")[0] + '.csv', encoding='utf-8', index=False)
 
         print("Processed " + file.split("/")[-1])
